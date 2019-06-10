80309c242f41a7a08020d5d7cbb30fa3fa0ce550
Ryan Haskell-Glatz <ryan.nhg@gmail.com>
2019-06-09 22:56:14 -0500

    jeebus take the wheel

--- src/Main.elm
+++ src/Main.elm
@@ -18,90 +18,48 @@ content =
     , bikes =
         { left =
             Bike
-                "Smart Bike"
+                "Smart Trainer"
                 "/public/bike.png"
-                [ Tier "3 Features"
-                    1000
-                    [ Feature "Digital braking —\u{00A0}ergonomic buttons reflected in digital experience" 50
-                    , Feature "Handsfree headset and group chat capability" 100
-                    , Feature "22” 1080p touchscreen" 250
-                    ]
-                , Tier "5 Features"
-                    1500
-                    [ Feature "Digital braking —\u{00A0}ergonomic buttons reflected in digital experience" 50
-                    , Feature "GPS headset and group chat capability" 100
-                    , Feature "22” 1080p touchscreen" 250
-                    , Feature "Interchangeable seat and pedals — ability to use standard outdoor bike options" 150
-                    , Feature "Digital steering — ergonomic buttons reflected in  digital experience" 500
-                    ]
-                , Tier "7 Features"
-                    2000
-                    [ Feature "Ultra braking —\u{00A0}ergonomic buttons reflected in digital experience" 150
-                    , Feature "Bluetooth headset and group chat capability" 1000
-                    , Feature "55” 1080p touchscreen" 250
-                    , Feature "Interchangeable seat and pedals — ability to use standard outdoor bike options" 150
-                    , Feature "Digital steering — ergonomic buttons reflected in  digital experience" 500
-                    , Feature "Robotic Hamster" 400
-                    , Feature "Game of Thrones Season 9" 5000
-                    ]
-                ]
+                bikeOptions
         , right =
             Bike
-                "Smart Trainer"
+                "Smart Bike"
                 "/public/machine.png"
-                [ Tier "3 Features"
-                    2600
-                    [ Feature "Digital braking —\u{00A0}ergonomic buttons reflected in digital experience" 50
-                    , Feature "Handsfree headset and group chat capability" 100
-                    , Feature "22” 1080p touchscreen" 250
-                    ]
-                , Tier "5 Features"
-                    2800
-                    [ Feature "Digital braking —\u{00A0}ergonomic buttons reflected in digital experience" 50
-                    , Feature "GPS headset and group chat capability" 100
-                    , Feature "22” 1080p touchscreen" 250
-                    , Feature "Interchangeable seat and pedals — ability to use standard outdoor bike options" 150
-                    , Feature "Digital steering — ergonomic buttons reflected in  digital experience" 500
-                    ]
-                , Tier "7 Features"
-                    3100
-                    [ Feature "Ultra braking —\u{00A0}ergonomic buttons reflected in digital experience" 150
-                    , Feature "Bluetooth headset and group chat capability" 1000
-                    , Feature "55” 1080p touchscreen" 250
-                    , Feature "Interchangeable seat and pedals — ability to use standard outdoor bike options" 150
-                    , Feature "Digital steering — ergonomic buttons reflected in  digital experience" 500
-                    , Feature "Robotic Hamster" 400
-                    , Feature "Game of Thrones Season 9" 5000
-                    ]
-                ]
+                bikeOptions
         }
     }
 
 
 bikeOptions =
-    [ Tier "3 Features"
+    [ Tier "+3 Features"
         2600
-        [ Feature "Digital braking —\u{00A0}ergonomic buttons reflected in digital experience" 50
+        [ Feature "Digital braking — ergonomic buttons reflected in  digital experience" 50
         , Feature "Handsfree headset and group chat capability" 100
-        , Feature "22” 1080p touchscreen" 250
+        , Feature "17” 1080p touchscreen" 250
+        , Feature "Interchangeable seat and pedals — ability to use standard outdoor bike options" 200
+        , Feature "Digital steering — ergonomic buttons reflected in  digital experience" 300
+        , Feature "Side to side motion — to better simulate climbs and sprints" 150
+        , Feature "LED lights — reflect in-game interactions" 200
+        , Feature "Incline — simulated grade with resistance" 50
+        , Feature "Biometric Fan — fan that syncs with your bpm, power, or cadence" 150
+        , Feature "Gaming buttons — quick-hit buttons that power in-game actions" 200
+        , Feature "Digital shifting — ergonomic buttons reflected in  digital experience" 300
         ]
-    , Tier "5 Features"
+    , Tier "+5 Features"
         2800
-        [ Feature "Digital braking —\u{00A0}ergonomic buttons reflected in digital experience" 50
-        , Feature "GPS headset and group chat capability" 100
-        , Feature "22” 1080p touchscreen" 250
-        , Feature "Interchangeable seat and pedals — ability to use standard outdoor bike options" 150
-        , Feature "Digital steering — ergonomic buttons reflected in  digital experience" 500
+        [ Feature "Digital braking — ergonomic buttons reflected in  digital experience" 50
+        , Feature "Handsfree headset and group chat capability" 100
+        , Feature "17” 1080p touchscreen" 250
+        , Feature "Interchangeable seat and pedals — ability to use standard outdoor bike options" 200
+        , Feature "Digital steering — ergonomic buttons reflected in  digital experience" 300
         ]
-    , Tier "7 Features"
+    , Tier "+7 Features"
         3100
-        [ Feature "Ultra braking —\u{00A0}ergonomic buttons reflected in digital experience" 150
-        , Feature "Bluetooth headset and group chat capability" 1000
-        , Feature "55” 1080p touchscreen" 250
-        , Feature "Interchangeable seat and pedals — ability to use standard outdoor bike options" 150
-        , Feature "Digital steering — ergonomic buttons reflected in  digital experience" 500
-        , Feature "Robotic Hamster" 400
-        , Feature "Game of Thrones Season 9" 5000
+        [ Feature "Digital braking — ergonomic buttons reflected in  digital experience" 50
+        , Feature "Handsfree headset and group chat capability" 100
+        , Feature "17” 1080p touchscreen" 250
+        , Feature "Interchangeable seat and pedals — ability to use standard outdoor bike options" 200
+        , Feature "Digital steering — ergonomic buttons reflected in  digital experience" 300
         ]
     ]
 
@@ -133,7 +91,6 @@ type alias Model =
     , imagesVisible : Bool
     , textVisible : Bool
     , areFeaturesVisible : Bool
-    , selectedOption : Maybe Selection
     }
 
 
@@ -145,7 +102,20 @@ type alias Selection =
 
 type Page
     = Homepage
-    | Pane Direction
+    | Pane Direction DetailPage
+
+
+type DetailPage
+    = Features Selection
+    | PremiumFeatures (List Bundle)
+    | Packages (Maybe Bundle)
+
+
+type alias Bundle =
+    { title : String
+    , description : String
+    , price : Int
+    }
 
 
 type Direction
@@ -159,8 +129,10 @@ type Msg
     | SelectPane Direction
     | GoHome
     | ShowFeatures
-    | SelectTier Tier
-    | ToggleFeature Selection Feature
+    | SelectTier Direction Tier
+    | ToggleFeature Direction Selection Feature
+    | ToPremiumFeatures Direction
+    | ToPackages Direction
 
 
 main : Program Flags Model Msg
@@ -182,7 +154,6 @@ init _ =
         False
         False
         False
-        Nothing
     , delay 300 ShowPanes
     )
 
@@ -200,12 +171,12 @@ view model =
             ]
             [ div
                 [ class "panes__side panes__side--left"
-                , classList [ ( "panes__side--ready", model.panesVisible && model.page /= Pane Right ) ]
+                , classList [ ( "panes__side--ready", model.panesVisible && isNotDirection Right model.page ) ]
                 ]
                 [ square "left" Left content.bikes.left model ]
             , div
                 [ class "panes__side panes__side--right"
-                , classList [ ( "panes__side--ready", model.panesVisible && model.page /= Pane Left ) ]
+                , classList [ ( "panes__side--ready", model.panesVisible && isNotDirection Left model.page ) ]
                 ]
                 [ square "right" Right content.bikes.right model ]
             ]
@@ -235,38 +206,150 @@ view model =
             [ div
                 [ class "features__container"
                 , classList
-                    [ ( "features__container--left", model.page == Pane Left )
+                    [ ( "features__container--left", isDirection Left model.page )
                     ]
                 ]
-                (case model.selectedOption of
-                    Just option ->
-                        case model.page of
-                            Pane pane ->
-                                case pane of
+                (case model.page of
+                    Pane dir detailPage ->
+                        let
+                            bike =
+                                case dir of
                                     Left ->
-                                        [ div [ class "labels" ]
-                                            [ div [ class "labels__label" ] [ text content.bikes.left.label ]
-                                            , div [ class "labels__total" ] [ text (totalPrice option) ]
-                                            ]
-                                        , viewFeaturesFor option content.bikes.left
-                                        ]
+                                        content.bikes.left
 
                                     Right ->
-                                        [ viewFeaturesFor option content.bikes.right
-                                        , div [ class "labels" ]
-                                            [ div [ class "labels__label" ] [ text content.bikes.right.label ]
-                                            , div [ class "labels__total" ] [ text (totalPrice option) ]
-                                            ]
-                                        ]
+                                        content.bikes.right
+                        in
+                        case detailPage of
+                            Features selection ->
+                                viewDetailPage dir bike selection totalPrice viewFeaturesFor (Just ToPremiumFeatures)
 
-                            Homepage ->
-                                []
+                            PremiumFeatures selectedBundles ->
+                                viewDetailPage dir bike selectedBundles premiumTotalPrice viewPremiumFeatures (Just ToPackages)
 
-                    Nothing ->
+                            Packages selectedBundle ->
+                                viewDetailPage dir bike selectedBundle packageTotalPrice viewPackages Nothing
+
+                    Homepage ->
                         []
                 )
             ]
         ]
+
+
+isNotDirection : Direction -> Page -> Bool
+isNotDirection dir page =
+    case page of
+        Homepage ->
+            True
+
+        Pane dir_ _ ->
+            dir_ /= dir
+
+
+isDirection : Direction -> Page -> Bool
+isDirection dir page =
+    case page of
+        Homepage ->
+            False
+
+        Pane dir_ _ ->
+            dir_ == dir
+
+
+viewDetailPage :
+    Direction
+    -> Bike
+    -> a
+    -> (a -> String)
+    -> (Direction -> Bike -> a -> Html Msg)
+    -> Maybe (Direction -> Msg)
+    -> List (Html Msg)
+viewDetailPage dir bike data priceFunction viewFunction nextMsg =
+    let
+        labelHtml =
+            div [ class "labels" ]
+                [ div [ class "labels__label" ] [ text bike.label ]
+                , div [ class "labels__total" ] [ text (priceFunction data) ]
+                ]
+
+        nextButton =
+            div
+                ([ style "margin-top" "3rem"
+                 , style "display" "flex"
+                 , style "justify-content" "flex-end"
+                 , style "position" "fixed"
+                 , style "bottom" "2rem"
+                 ]
+                    ++ (case dir of
+                            Left ->
+                                [ style "left" "calc(50% + 2rem)" ]
+
+                            Right ->
+                                [ style "right" "calc(50% + 2rem)" ]
+                       )
+                )
+                [ button
+                    (case nextMsg of
+                        Just msg_ ->
+                            [ class "feature__tier", onClick (msg_ dir) ]
+
+                        Nothing ->
+                            [ class "feature__tier" ]
+                    )
+                    [ text "Next" ]
+                ]
+    in
+    case dir of
+        Left ->
+            [ labelHtml
+            , viewFunction dir bike data
+            , nextButton
+            ]
+
+        Right ->
+            [ viewFunction dir bike data
+            , labelHtml
+            , nextButton
+            ]
+
+
+
+-- VIEW: PREMIUM FEATURES
+
+
+viewPremiumFeatures : Direction -> Bike -> List Bundle -> Html Msg
+viewPremiumFeatures dir bike selectedBundles =
+    text "Premium Features"
+
+
+premiumTotalPrice : List Bundle -> String
+premiumTotalPrice bundles =
+    bundles
+        |> List.map .price
+        |> List.sum
+        |> String.fromInt
+
+
+
+-- VIEW: PACKAGES
+
+
+viewPackages : Direction -> Bike -> Maybe Bundle -> Html Msg
+viewPackages dir bike selectedBundle =
+    text "Packages"
+
+
+packageTotalPrice : Maybe Bundle -> String
+packageTotalPrice bundle =
+    bundle
+        |> Maybe.map .price
+        |> Maybe.withDefault 0
+        |> String.fromInt
+
+
+
+-- VIEW: FEATURES
 
 
 totalPrice : Selection -> String
@@ -277,21 +360,23 @@ totalPrice selection =
         |> (\price -> "$" ++ price)
 
 
-viewFeaturesFor : Selection -> Bike -> Html Msg
-viewFeaturesFor selection bike =
+viewFeaturesFor : Direction -> Bike -> Selection -> Html Msg
+viewFeaturesFor dir bike selection =
     div [ class "feature" ]
-        [ div [ class "feature__tiers" ]
-            (List.map (viewTier selection) bike.options)
+        [ div [ class "feature__section-label" ] [ text "Choose your tier" ]
+        , div [ class "feature__tiers" ]
+            (List.map (viewTier dir selection) bike.options)
+        , div [ class "feature__section-label" ] [ text "Choose your basic features" ]
         , div [ class "feature__features" ]
-            (List.map (viewFeature selection) selection.tier.features)
+            (List.map (viewFeature dir selection) selection.tier.features)
         ]
 
 
-viewTier : Selection -> Tier -> Html Msg
-viewTier selection tier =
+viewTier : Direction -> Selection -> Tier -> Html Msg
+viewTier dir selection tier =
     button
         [ class "feature__tier"
-        , onClick (SelectTier tier)
+        , onClick (SelectTier dir tier)
         , classList
             [ ( "feature__tier--selected", selection.tier == tier )
             ]
@@ -300,12 +385,12 @@ viewTier selection tier =
         ]
 
 
-viewFeature : Selection -> Feature -> Html Msg
-viewFeature selection feature =
+viewFeature : Direction -> Selection -> Feature -> Html Msg
+viewFeature dir selection feature =
     div []
         [ button
             [ class "feature__feature"
-            , onClick (ToggleFeature selection feature)
+            , onClick (ToggleFeature dir selection feature)
             , classList
                 [ ( "feature__feature--selected", List.member feature selection.features )
                 ]
@@ -315,6 +400,10 @@ viewFeature selection feature =
         ]
 
 
+
+-- VIEW - HOMEPAGE
+
+
 square : String -> Direction -> Bike -> Model -> Html Msg
 square squareModifier dir bike model =
     div [ class "panes__square-wrapper", onClick (SelectPane dir) ]
@@ -322,7 +411,7 @@ square squareModifier dir bike model =
             [ class ("panes__square panes__square--" ++ squareModifier)
             , classList
                 [ ( "panes__square--visible", model.imagesVisible )
-                , ( "panes__square--zoomed", model.page == Pane dir )
+                , ( "panes__square--zoomed", isDirection dir model.page )
                 ]
             , style "background-image" ("url(" ++ bike.image ++ ")")
             ]
@@ -330,7 +419,7 @@ square squareModifier dir bike model =
         , div
             [ class "panes__square-text"
             , classList
-                [ ( "panes__square-text--offscreen", model.page == Pane dir )
+                [ ( "panes__square-text--offscreen", isDirection dir model.page )
                 ]
             ]
             [ text bike.label
@@ -343,6 +432,10 @@ square squareModifier dir bike model =
                 |> text
             ]
         ]
+
+
+
+-- UPDATE
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
@@ -360,14 +453,13 @@ update msg model =
 
         SelectPane dir ->
             ( { model
-                | page = Pane dir
+                | page = Pane dir (Features (optionsFor dir))
                 , color =
                     if dir == Right then
                         "#000a53"
 
                     else
                         "#11b3ba"
-                , selectedOption = Just (optionsFor dir)
               }
             , delay 300 ShowFeatures
             )
@@ -376,7 +468,6 @@ update msg model =
             ( { model
                 | page = Homepage
                 , areFeaturesVisible = False
-                , selectedOption = Nothing
               }
             , Cmd.none
             )
@@ -386,13 +477,23 @@ update msg model =
             , Cmd.none
             )
 
-        SelectTier tier ->
-            ( { model | selectedOption = Just (Selection tier []) }
+        SelectTier dir tier ->
+            ( { model | page = Pane dir (Features (Selection tier [])) }
             , Cmd.none
             )
 
-        ToggleFeature selection feature ->
-            ( { model | selectedOption = Just { selection | features = toggleFeature feature selection.features } }
+        ToggleFeature dir selection feature ->
+            ( { model | page = Pane dir (Features { selection | features = toggleFeature feature selection.features }) }
+            , Cmd.none
+            )
+
+        ToPremiumFeatures dir ->
+            ( { model | page = Pane dir (PremiumFeatures []) }
+            , Cmd.none
+            )
+
+        ToPackages dir ->
+            ( { model | page = Pane dir (Packages Nothing) }
             , Cmd.none
             )
 
